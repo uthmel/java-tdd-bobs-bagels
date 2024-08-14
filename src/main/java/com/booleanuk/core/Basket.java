f6a47aa7b5a38c8f9f6259cd07d34bc5c3384f52
uthmel <melvin.uthayaseelan@no.experis.com>
2024-08-14 13:36:38 +0200

    adding new test and core source

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,51 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class Basket {
-    int test = 0;
+    private ArrayList<String> items;
+    private int capacity;
+
+
+    public Basket(int capacity) {
+        this.items = new ArrayList<>();
+        this.capacity = capacity;
+    }
+
+    public Basket(){
+        this(10);
+    }
+
+
+    public boolean isFull() {
+        return items.size() >=capacity;
+
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public boolean addBagel(String bagel){
+        if (isFull()) {
+            return true;
+        }
+
+        if (items.contains(bagel)){
+            return false;
+        }
+
+        items.add(bagel);
+        return true;
+    }
+
+     public boolean removeBagel(String bagel){
+        if (items.contains(bagel)) {
+            items.remove(bagel);
+            return true;
+        }
+
+        return false;
+    }
+
 }
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -3,13 +3,38 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 class BasketTest {
 
+
+
     @Test
-    public void testAddBagelToBasket(){
-        Basket basket = new Basket();
+    public void testAddBagelToBasketIfNotFull(){
+        Basket basket = new Basket(10);
         boolean result = basket.addBagel("Plain Bagel");
         Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void testAddBagelToBasketIfFull(){
+        Basket basket = new Basket(15);
+        boolean result = basket.addBagel("Plain Bagel");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void testRemoveBagelFromBasket() {
+
+
+    }
+
+    @Test
+    public void testShouldNotAddDuplicateBagel() {
+        Basket basket = new Basket();
+        basket.addBagel("Plain Bagel");
+        boolean result = basket.addBagel("Plain Bagel");
+        Assertions.assertFalse(result);
     }
 
 }
